822d6ac454ad0b333360e2f3175988473df9e5bc
evanhwz <evan.haryo@ui.ac.id>
2026-02-08 14:32:13 +0700

    feat: implement update on service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -37,4 +37,10 @@ public class ProductServiceImpl implements ProductService {
     public Product findById(String id){
         return productRepository.findById(id);
     }
+
+    @Override
+    public Product update(Product product){
+        return productRepository.update(product);
+    }
+
 }
